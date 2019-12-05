2b01acc070d032a3c15cd4b5546e9550ebace753
morishita <t.m.thp.hnk@gmail.com>
2019-12-05 19:17:05 +0900

    a

---
+++ ABC/ABC004/B - 回転.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define sz(x) ((ll)(x).size())
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  vector<vector<char>> c(4, vector<char>(4));
+  rep(i, 4) {
+    rep(j, 4) {
+      cin >> c[i][j];
+    }
+  }
+
+  vector<vector<char>> ans(4, vector<char>(4));
+  rep(i, 4) {
+    rep(j, 4) {
+      ans[i][j] = c[3 - i][3 - j];
+    }
+  }
+  rep(i, sz(ans)) {
+    rep(j, sz(ans[0])) {
+      cout << ans[i][j];
+
+      cout << (j == sz(ans[0]) - 1 ? "\n" : " ");
+    }
+  }
+}
