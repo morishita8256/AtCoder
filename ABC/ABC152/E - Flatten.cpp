a65d9d265727d3ae20cd41ba189d82f21713797d
morishita <t.m.thp.hnk@gmail.com>
2020-01-19 22:44:18 +0900

    a

---
+++ ABC/ABC152/E - Flatten.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+
+/* mint、表示は変数名.x */
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x % MOD + MOD) % MOD) {
+  }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= MOD)
+      x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += MOD - a.x) >= MOD)
+      x -= MOD;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  /* 変数名.pow(n) */
+  mint pow(ll t) const {
+    if (!t)
+      return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1)
+      a *= *this;
+    return a;
+  }
+
+  // 以降はMODが素数のみ
+  mint inv() const {
+    return pow(MOD - 2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+
+map<int, int> prime_factor(int n) {
+  map<int, int> res;
+  for (int i = 2; i * i <= n; i++) {
+    while (n % i == 0) {
+      ++res[i];
+      n /= i;
+    }
+  }
+  if (n != 1)
+    res[n] = 1;
+  return res;
+}
+
+/* a^n (MOD) の計算 O(log n) */
+ll modpow(ll a, ll n) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1)
+      res = res * a % MOD;
+    a = a * a % MOD;
+    n >>= 1;
+  }
+  return res;
+}
+
+signed main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  rep(i, N) {
+    cin >> A[i];
+  }
+
+  map<int, int> factor;
+  vector<map<int, int>> mp(N);
+
+
+  rep(i, N) {
+    mp[i] = prime_factor(A[i]);
+    for (pair<int, int> item : mp[i]) {
+      int Fi = item.fi;
+      int Se = item.se;
+      chmax(factor[Fi], Se);
+    }
+  }
+
+  mint multi = 1;
+  for (auto item : factor) {
+    auto Fi = item.fi;
+    auto Se = item.se;
+    multi *= modpow(Fi, Se);
+  }
+
+  mint ans = 0;
+  rep(i, N) {
+    mint temp = multi;
+    for (pair<int, int> item : mp[i]) {
+      int Fi = item.fi;
+      int Se = item.se;
+      temp /= modpow(Fi, Se);
+    }
+    ans += temp;
+  }
+  cout << ans.x << "\n";
+}
---
+++ ABC/ABC152/F - Tree and Constraints.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+typedef long long ll;
+const int INF = 1001001001;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+__attribute__((constructor)) void initial() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+}
+
+signed main() {
+  int N;
+  cin >> N;
+  vector<int> a(N - 1), b(N - 1);
+  rep(i, N - 1) {
+    cin >> a[i] >> b[i];
+    a[i]--, b[i]--;
+  }
+  int M;
+  cin >> M;
+  vector<int> u(M), v(M);
+  rep(i, M) {
+    cin >> u[i] >> v[i];
+    u[i]--, v[i]--;
+  }
+}
