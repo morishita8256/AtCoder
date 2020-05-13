b5a58581b9104b6daf74db36ab5a75dafaffdb40
morishita <t.m.thp.hnk@gmail.com>
2020-05-14 02:31:32 +0900

    a

---
+++ ARC/ARC028/B - 特別賞.cpp
@@ -0,0 +1,100 @@
+#pragma region head
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+#define debug(var) cout << "[" << #var << "]\n" << var << endl
+#define int long long
+typedef long long ll;
+const int INF = 1001001001001001001ll;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<T>& v) {
+  int len = v.size();
+  for (int i = 0; i < len; ++i) {
+    s << v[i];
+    if (i < len - 1)
+      s << ' ';
+  }
+  return s;
+}
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<vector<T>>& vv) {
+  int len = vv.size();
+  for (int i = 0; i < len; ++i) {
+    s << vv[i];
+    if (i != len - 1)
+      s << '\n';
+  }
+  return s;
+}
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+__attribute__((constructor)) void initial() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+}
+#pragma endregion
+
+signed main() {
+  int N, K;
+  cin >> N >> K;
+  vector<int> X(N);
+  vector<int> age2rank(N);
+  rep(i, N) {
+    cin >> X[i];
+    X[i]--;
+    age2rank[X[i]] = i;
+  }
+
+  vector<int> ans;
+  vector<bool> removed(N, false);
+  int ind = K - 1;
+  int rank = N - 1;
+  rep(_, N - K + 1) {
+    ans.pb(age2rank[ind] + 1);
+
+    int rm_ind = X[rank--];
+    removed[rm_ind] = true;
+    if (rm_ind <= ind)
+      ind++;
+
+    while (removed[ind])
+      ind++;
+  }
+
+  reverse(all(ans));
+  rep(i, len(ans)) {
+    cout << ans[i] << endl;
+  }
+}
