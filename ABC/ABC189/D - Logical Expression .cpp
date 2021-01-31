723f32611531dc886f4e45740c888d96cb6cee50
morishita <t.m.thp.hnk@gmail.com>
2021-01-31 11:14:19 +0900

    .

---
+++ ABC/ABC189/D - Logical Expression .cpp
@@ -0,0 +1,97 @@
+#pragma region head
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+#define debug(var) cout << "[" << #var << "]\n" << var << endl
+#define test(s) \
+  if (!(s))     \
+    cout << "Line " << __LINE__ << ": [" << #s << "] is false" << endl;
+#define int long long
+typedef long long ll;
+#define double long double
+typedef double ld;
+const int INF = 1001001001001001001ll;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<T>& v) {
+  int len = v.size();
+  for (int i = 0; i < len; ++i) {
+    s << v[i];
+    if (i < len - 1)
+      s << ' ';
+  }
+  return s;
+}
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<vector<T>>& vv) {
+  int len = vv.size();
+  for (int i = 0; i < len; ++i) {
+    s << vv[i];
+    if (i != len - 1)
+      s << '\n';
+  }
+  return s;
+}
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+__attribute__((constructor)) void initial() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+}
+#pragma endregion
+
+signed main() {
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  rep(i, N) {
+    cin >> S[i];
+  }
+
+  vector<int> T(N + 1), F(N + 1);
+  T[N] = 1;
+
+  repr(i, N - 1, 0) {
+    if (S[i] == "OR") {
+      T[i] += T[i + 1] * 2;
+      F[i] = T[i + 1] + F[i + 1];
+    } else {
+      F[i] += F[i + 1] * 2;
+      T[i] = T[i + 1] + F[i + 1];
+    }
+  }
+  int ans = T[0] + F[0];
+  cout << ans << endl;
+}
