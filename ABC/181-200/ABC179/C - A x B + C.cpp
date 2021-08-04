4eb4dc04507156ff9585aa8c17ac33efb36d4678
morishita4632 <t.m.thp.hnk@gmail.com>
2021-08-04 11:24:40 +0900

    .

---
+++ ABC/181-200/ABC179/C - A x B + C.cpp
@@ -0,0 +1,97 @@
+#pragma region head
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+#define debug(var) cout << "[" << #var << "]\n" << var << endl
+#define test(s) \
+  if (!(s))     \
+    cout << "Line " << __LINE__ << ": [" << #s << "] is false" << endl;
+#define int long long
+typedef long long ll;
+#define double long double
+typedef double ld;
+const int INF = 1001001001001001001ll;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<T>& v) {
+  int len = v.size();
+  for (int i = 0; i < len; ++i) {
+    s << v[i];
+    if (i < len - 1)
+      s << ' ';
+  }
+  return s;
+}
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<vector<T>>& vv) {
+  int len = vv.size();
+  for (int i = 0; i < len; ++i) {
+    s << vv[i];
+    if (i != len - 1)
+      s << '\n';
+  }
+  return s;
+}
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+__attribute__((constructor)) void initial() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+}
+#pragma endregion
+
+vector<ll> divisor(ll n) {
+  vector<ll> ret;
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      ret.push_back(i);
+      if (i * i != n)
+        ret.push_back(n / i);
+    }
+  }
+  sort(begin(ret), end(ret));
+  return (ret);
+}
+
+signed main() {
+  int N;
+  cin >> N;
+
+  int ans = 0;
+  repp(A, 1, N) {
+    ans += (N - 1) / A;
+  }
+  cout << ans << endl;
+}
