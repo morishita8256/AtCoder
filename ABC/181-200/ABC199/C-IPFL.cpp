483d264f75abae7fd1c9b7b41989003e4be9f4cd
morishita4632 <t.m.thp.hnk@gmail.com>
2022-03-01 15:47:34 +0900

    .

---
+++ ABC/181-200/ABC199/C-IPFL.cpp
@@ -0,0 +1,113 @@
+#pragma region head
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+#define debug(var) cout << "[" << #var << "]\n" << var << endl
+#define test(s) \
+  if (!(s))     \
+    cout << "Line " << __LINE__ << ": [" << #s << "] is false" << endl;
+#define int long long
+typedef long long ll;
+#define double long double
+typedef double ld;
+const int INF = 1001001001001001001ll;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<T>& v) {
+  int len = v.size();
+  for (int i = 0; i < len; ++i) {
+    s << v[i];
+    if (i < len - 1)
+      s << ' ';
+  }
+  return s;
+}
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<vector<T>>& vv) {
+  int len = vv.size();
+  for (int i = 0; i < len; ++i) {
+    s << vv[i];
+    if (i != len - 1)
+      s << '\n';
+  }
+  return s;
+}
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+__attribute__((constructor)) void initial() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+}
+#pragma endregion
+
+signed main() {
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+  int Q;
+  cin >> Q;
+  vector<int> T(Q), A(Q), B(Q);
+  rep(i, Q) {
+    cin >> T[i] >> A[i] >> B[i];
+    A[i]--, B[i]--;
+  }
+
+  // 0~N-1, N~2N-1
+
+  bool flip = false;
+  rep(q, Q) {
+    if (T[q] == 1) {
+      if (flip) {
+        if (A[q] < N)
+          A[q] += N;
+        else
+          A[q] -= N;
+        if (B[q] < N)
+          B[q] += N;
+        else
+          B[q] -= N;
+      }
+      swap(S[A[q]], S[B[q]]);
+    } else {
+      flip = !flip;
+    }
+  }
+
+  if (flip)
+    S = S.substr(N, N) + S.substr(0, N);
+
+  cout << S << endl;
+}
