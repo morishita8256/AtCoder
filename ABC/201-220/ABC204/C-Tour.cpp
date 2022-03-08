b7629cf74a36c6004f0b891d3bb966467257225e
morishita4632 <t.m.thp.hnk@gmail.com>
2022-03-08 10:33:10 +0900

    .

---
+++ ABC/201-220/ABC204/C-Tour.cpp
@@ -0,0 +1,111 @@
+#pragma region head
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repp(i, a, b) for (int i = a; i <= (b); ++i)
+#define repr(i, a, b) for (int i = a; i >= (b); --i)
+#define bit(n) (1LL << (n))
+#define len(x) ((ll)(x).size())
+#define debug(var) cout << "[" << #var << "]\n" << var << endl
+#define test(s) \
+  if (!(s))     \
+    cout << "Line " << __LINE__ << ": [" << #s << "] is false" << endl;
+#define int long long
+typedef long long ll;
+#define double long double
+typedef double ld;
+const int INF = 1001001001001001001ll;
+const ll LINF = 1001001001001001001ll;
+const int MOD = 1000000007;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<T>& v) {
+  int len = v.size();
+  for (int i = 0; i < len; ++i) {
+    s << v[i];
+    if (i < len - 1)
+      s << ' ';
+  }
+  return s;
+}
+
+template <typename T>
+ostream& operator<<(ostream& s, const vector<vector<T>>& vv) {
+  int len = vv.size();
+  for (int i = 0; i < len; ++i) {
+    s << vv[i];
+    if (i != len - 1)
+      s << '\n';
+  }
+  return s;
+}
+
+template <class T>
+inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <class T>
+inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+__attribute__((constructor)) void initial() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(15);
+}
+#pragma endregion
+
+int N, M;
+vector<vector<int>> G;
+vector<int> D;
+
+void dfs(int s) {
+  for (int g : G[s]) {
+    if (D[g])
+      continue;
+    else {
+      D[g] = true;
+      dfs(g);
+    }
+  }
+}
+
+signed main() {
+  cin >> N >> M;
+  G.resize(N);
+  D.resize(N);
+
+  vector<int> A(M), B(M);
+  rep(i, M) {
+    cin >> A[i] >> B[i];
+    A[i]--, B[i]--;
+    G[A[i]].pb(B[i]);
+  }
+
+  int ans = 0;
+  rep(i, N) {
+    rep(j, N) D[j] = false;
+    D[i] = true;
+    dfs(i);
+    rep(j, N) ans += D[j];
+  }
+
+  cout << ans << endl;
+}
